3fb82d3279eab9b5eeda594da564e4078245a1d3
Atieh.Baratinia <atiehbaratinia@gmail.com>
2020-10-31 19:01:35 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from lexer import Lexer
+# from parser import Parser
+
+if __name__ == "__main__":
+    lexer = Lexer().build()
+    file = open('test.txt')
+    text_input = file.read()
+    file.close()
+    lexer.input(text_input)
+    while True:
+        tok = lexer.token()
+        if not tok: break
+        print(tok)
+    # parser = Parser()
+    # parser.build().parse(text_input, lexer, False)
