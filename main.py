1fe150f9dee5ca81f6abdb8e27d9e581d4de1f0b
Atieh.Baratinia <atiehbaratinia@gmail.com>
2020-11-03 21:12:53 +0330

    bug fixed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from lexer import Lexer
 
 if __name__ == "__main__":
     lexer = Lexer().build()
-    file = open('test.txt')
+    file = open('test1.txt')
     text_input = file.read()
     file.close()
     lexer.input(text_input)
