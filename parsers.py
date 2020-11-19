d997098cfce7fcae5d1ac0dd7a8b814b7414cc06
ghazale <ghazalnoroozi27@yahoo.com>
2020-11-19 14:59:05 +0330

    Complete the basic grammar

--- parsers.py
+++ parsers.py
@@ -3,6 +3,7 @@ from lexer import Lexer
 # from nonTerminal import NonTerminal
 # from codeGenerator import CodeGenerator
 
+
 class Parser:
 
     tokens = Lexer().tokens
@@ -59,12 +60,10 @@ class Parser:
                    | FUNCTION ID LRB paramdecs RRB block"""
         print("funcdec : FUNCTION ID LRB paramdecs RRB COLON type block | FUNCTION ID LRB paramdecs RRB block")
 
-
     def p_paramdecs(self, p):
         """paramdecs : paramdecslist
                      | epsilon"""
         print("paramdecs : paramdecslist | epsilon")
-
 
     def p_paramdecslist(self, p):
         """paramdecslist : paramdec
@@ -76,6 +75,68 @@ class Parser:
                     | ID LSB RSB COLON type"""
     print("paramdec : ID COLON type | ID LSB RSB COLON type")
 
+
+    def p_block(self, p):
+        "block : { stimtlist }"
+        print("block : { stimtlist }")
+
+    def p_stmtlist(self, p):
+        "stmlist : stmt | stmlist stmt | epsilon"
+        print("stmlist : stmt | stmlist stmt | epsilon")
+
+    def p_lvalue(self, p):
+        "lvalue : id | id [ exp ]"
+        print("lvalue : id | id [ exp ]")
+
+    def p_case(self, p):
+        "case : where const : stmtlist"
+        print("case : where const : stmtlist")
+
+    def p_cases(self, p):
+        "cases : case | cases case | epsilon"
+        print("cases : case | cases case | epsilon")
+
+    def p_stmt(self, p):
+        """stmt : return exp ; | exp ; | block | vardec |
+        while ( exp ) stmt | on ( exp ) { cases } ; |
+        for ( exp ; exp ; exp ) stmt | for ( id in id ) stmt |
+        if ( exp ) stmt elseiflist | if ( exp ) stmt elseiflist else stmt |
+        print ( id ) ;"""
+
+        print("""stmt : return exp ; | exp ; | block | vardec |
+        while ( exp ) stmt | on ( exp ) { cases } ; |
+        for ( exp ; exp ; exp ) stmt | for ( id in id ) stmt |
+        if ( exp ) stmt elseiflist | if ( exp ) stmt elseiflist else stmt |
+        print ( id ) ;""")
+
+    def p_elseiflist(self, p):
+        "elseiflist : elseif ( exp ) stmt | elseiflist elseif ( exp ) stmt | epsilon"
+
+    def p_relopexp(self, p):
+        "relocexp : exp reloc exp | relopexp relop exp"
+        print("relocexp : exp reloc exp | relopexp relop exp")
+
+    def p_exp(self, p):
+        """exp : lvalue = exp | exp operator exp | relopexp
+        const | lvalue | id ( explist ) | ( exp ) | id () | - exp | not exp"""
+        print("""exp : lvalue = exp | exp operator exp | relopex const | lvalue |
+         id ( explist ) | ( exp ) | id () | - exp | not exp""")
+
+    def p_operator(self, p):
+        "operator : and | or | + | - | * | / | %"
+        print("operator : and | or | + | - | * | / | %")
+
+    def p_const(self, p):
+        "const : intnumber | floatnumber | True | False"
+        print("const : intnumber | floatnumber | True | False")
+
+    def p_relop(self, p):
+        "relop : > | < | != | == | <= | >="
+        print("relop : > | < | != | == | <= | >=")
+
+    def p_explist(self, p):
+        "explist : exp | explist , exp"
+        print("explist : exp | explist , exp")
 
     def p_exp_sum(self, p):
         "exp : exp SUM exp"
@@ -98,6 +159,7 @@ class Parser:
         print("exp : INTEGERNUMBER")
         # p[0] = NonTerminal()
         # p[0].value = p[1]
+
 
     precedence = (
         ('left', 'SUM', 'SUB'),
